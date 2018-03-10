543f4881e513c523bde1bde2e5c399862b8eef43
thiagorinu@gmail.com <thiagorinu@gmail.com>
2018-03-10 00:08:45 -0300

    bug

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mydidata',
-
+    'ckeditor',
+    'ckeditor_uploader',
+    'storages',
 ]
 
 MIDDLEWARE = [
